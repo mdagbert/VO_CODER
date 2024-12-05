67ae32621fdc858a54cff6d3da4f08d180df4272
mdagbert <mathis.dagbert@edu.univ-fcomte.fr>
2024-12-05 14:59:47 +0100

    First Commit Base Method + StdAudio/Draw

---
+++ src/Pauvocoder.java
@@ -0,0 +1,27 @@
+public class Pauvocoder {
+    final int F = 44100;
+
+
+    public static void main(String[] args) {
+
+    }
+    public static double[] resample(double[] input, double freqScale) {
+
+    }
+    public static double[] echo(double[] input, double delayMs, double attn) {
+
+    }
+    public static double[] vocodeSimple(double[] input, double timeScale) {
+
+    }
+    public static double[] vocodeSimpleOver(double[] input, double timeScale) {
+
+    }
+    public static double[] vocodeSimpleOverCross(double[] input, double timeScale) {
+
+    }
+    public static void joue(double[] input) {
+
+    }
+
+}
